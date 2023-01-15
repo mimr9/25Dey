f61119ed56f14393011a703ba4e56daf79d8601e
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2023-01-15 19:50:08 +0330

    javascript for a simple menu activation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,8 @@
 hljs.highlightAll();
+
+let galilo = document.querySelectorAll('.menu-list li a');
+for(let i = 0; i < galilo.length; i++) {
+  galilo[i].addEventListener("click", function(){
+    this.parentElement.querySelector('.nested').classList.toggle('mActive');
+  });
+}
